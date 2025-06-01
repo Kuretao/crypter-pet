f60c28b82896de6580f8d4e7e12863e9acd833ea
Kuretao <114327440+Kuretao@users.noreply.github.com>
2025-06-01 16:05:12 +0300

    Homepage - блок 1 html готов

--- src/components/home-page/block-1/block-1.jsx
+++ src/components/home-page/block-1/block-1.jsx
@@ -1,6 +1,8 @@
 import styles from './style.module.scss';
 import arrow from './../../../assets/images/arrow__big.svg'
 import sphere from './../../../assets/icons/sphere.svg'
+import slide1 from './../../../assets/images/img.png'
+import {Button} from "../../ui/Button/Button.jsx";
 
 const HomeWelcomeHeader = () =>{
     return (
@@ -22,9 +24,44 @@ const HomeWelcomeHeader = () =>{
 }
 
 const HomeWelcomeMain = () =>{
+    const info = [
+        {
+            title: 'Collection',
+            content: 'Escape II'
+        },
+        {
+            title: 'Collection',
+            content: 'Escape II'
+        },
+        {
+            title: 'Collection',
+            content: 'Escape II'
+        },
+    ]
     return (
-        <div>
+        <div className={styles.main}>
+            <div className={styles["main-slider"]}>
+                <img src={slide1} alt="main-img"/>
+            </div>
+            <div className={styles["main-content"]}>
+                <ul className={styles["main-content-info"]}>
+                    {info.map((item, index) => (
+                        <li key={index}>
+                            <span>{item.title}</span>
+                            <span><img src={sphere} alt="sphere"/> {item.content}</span>
+                        </li>
+                    ))}
+                </ul>
 
+                <div>
+                    <h2>The creator <br/> network.</h2>
+
+                    <div className={styles.btns}>
+                        <Button text={'view nft'} additionalClass={'transparent'}/>
+                        <Button text={'place a bid'} additionalClass={'dark'}/>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
